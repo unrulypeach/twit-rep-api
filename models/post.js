f9e48cda6db05aee96c7e80ff3f79103b293d33f
lc <gingeraleisthebest@gmail.com>
2023-11-21 18:48:26 -0500

    updated post schema

--- models/post.js
+++ models/post.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = require('./user');
+
+/* const PostSchema = new Schema({
+  content: String,
+  image: String,
+  date: { type: Date, default: Date.now },
+  uid: { type: Schema.Types.ObjectId, ref: User, required: true },
+  postid: Schema.Types.ObjectId, // id of post being replied to
+  level: { type: Number, default: 0 }, // ?? not sure how to implement this, how to add number to replies
+}); */
 
 const PostSchema = new Schema({
   content: String,
   image: String,
   date: { type: Date, default: Date.now },
-})
-
-module.exports = mongoose.model('Post', PostSchema)
+  uid: { type: Schema.Types.ObjectId, ref: User, required: true },
+  comments: [ Schema.Types.ObjectId ],
+});
+module.exports = mongoose.model('Post', PostSchema);
