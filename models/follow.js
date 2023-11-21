d2dfea69f26a82ec88b9369fc3a1d5180b15d3e0
lc <gingeraleisthebest@gmail.com>
2023-11-20 19:14:41 -0500

    created follow model

---
+++ models/follow.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const followSchema = new Schema({
+  user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  follow_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  follow_date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Follow', followSchema);
