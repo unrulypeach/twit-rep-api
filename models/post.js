0d75f5df51220bc1f6ad915e1288dcb5fa807e03
lc <gingeraleisthebest@gmail.com>
2023-11-21 22:31:01 -0500

    moved likes into post schema as an array

--- models/post.js
+++ models/post.js
@@ -2,20 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./user');
 
-/* const PostSchema = new Schema({
-  content: String,
-  image: String,
-  date: { type: Date, default: Date.now },
-  uid: { type: Schema.Types.ObjectId, ref: User, required: true },
-  postid: Schema.Types.ObjectId, // id of post being replied to
-  level: { type: Number, default: 0 }, // ?? not sure how to implement this, how to add number to replies
-}); */
-
 const PostSchema = new Schema({
   content: String,
   image: String,
   date: { type: Date, default: Date.now },
   uid: { type: Schema.Types.ObjectId, ref: User, required: true },
-  comments: [ Schema.Types.ObjectId ],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Post'}],
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
+
+PostSchema.virtual('likes_count').get(function() {
+  return this.likes.length;
+})
+
 module.exports = mongoose.model('Post', PostSchema);
