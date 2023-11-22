8fde6a8a1b821a2dc6baaa527feb7f54ed01d47a
lc <gingeraleisthebest@gmail.com>
2023-11-21 23:06:19 -0500

    finished implementing post fns

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,30 +2,111 @@ const Post = require('../models/post');
 const asyncHandler = require('express-async-handler');
 
 exports.get_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (GET) Post');
+  const postid = req.params.id;
+
+  const post = await Post.findById(postid).populate([{
+    path: 'uid',
+    select: { username: 1, userhandle: 1, profile_pic: 1}
+  }, {
+    path: 'comments',
+    populate: {
+      path: 'uid',
+      select: { username: 1, userhandle: 1, profile_pic: 1}    
+    }
+  }]).exec();
+
+  res.json({
+    post,
+    likes: post.likes_count,
+  });
 });
 
 exports.create_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (POST) Post');
-});
+  const { content, image } = req.body;
+  const user = req.user.uid;
 
-exports.delete_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (DELETE) Post');
-});
+  const newPost = new Post({
+    ...(content) && {content},
+    ...(image) && {image},
+    uid: user,
+  });
 
-exports.like_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (POST) Like');
-});
+  const postRes = await newPost.save();
 
-exports.unlike_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (DELETE) Like');
+  res.json(postRes);
 });
 
 exports.reply_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (POST) Reply');
+  const { content, image, postid } = req.body;
+  const user = req.user.uid;
+
+  const newPost = new Post({
+    ...(content) && {content},
+    ...(image) && {image},
+    uid: user,
+  });
+
+  // add comment to postid (previous post)
+  const postRes = await newPost.save();
+  const addComment = await Post.updateOne(
+    { _id: postid },
+    {
+      $push: {
+        comments: newPost._id
+        }
+    }
+  ).exec();
+
+  res.json({postRes, addComment});
 });
 
-// SHOULD THIS BE THE SAME AS GET_POST???
-exports.get_reply_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: (GET) Reply');
+exports.delete_post = asyncHandler(async (req, res, next) => {
+  const { postid } = req.body;
+
+  const delPost = await Post.findByIdAndDelete(postid).exec();
+
+  res.json(delPost)
+});
+
+exports.like_post = asyncHandler(async (req, res, next) => {
+  const { postid } = req.body;
+  const user = req.user.uid;
+
+  const updatePost = await Post.updateOne(
+    { _id: postid },
+    {
+      $addToSet: {
+        likes: user
+      }
+    }
+  ).exec();
+
+  res.json(updatePost);
+});
+
+exports.unlike_post = asyncHandler(async (req, res, next) => {
+  const { postid } = req.body;
+  const user = req.user.uid;
+
+  const updatePost = await Post.updateOne(
+    { _id: postid },
+    {
+      $pull: {
+        likes: user
+      }
+    }
+  ).exec();
+
+  res.json(updatePost);
+});
+
+exports.get_post_likes_list = asyncHandler(async (req, res, next) => {
+  const { postid } = req.body;
+
+  const likesList = await Post.findOne({_id: postid}).select('likes -_id').populate({
+    path: 'likes',
+    select: { username: 1, userhandle: 1, profile_pic: 1, _id: 0 }
+  }).exec();
+
+  res.json(likesList);
 });
